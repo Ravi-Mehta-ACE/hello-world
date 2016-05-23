b5f054fcb1f6a2eb1c674c6c669f286099f6591a
Ravi Mehta <Smartguy.ravimehta@gmail.com>
2016-05-23 18:07:08 +0530

    Hello World Project in swift containg stack view and table view tutorials. Added a location module which shows user's current location and a new view controller named 'MapViewController' which displays annotation of the selected restaurant

---
+++ LauncherViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LauncherViewController.swift
+//  HelloWorld
+//
+//  Created by Puneet Rao on 23/05/16.
+//  Copyright © 2016 Geeks. All rights reserved.
+//
+
+import Cocoa
+
+class LauncherViewController: UIViewController {
+
+}
